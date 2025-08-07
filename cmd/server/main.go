8fd067103ffed09ebac86cba54d8b3f8dc3e989d
Jodus-Melodus <philipgricius@gmail.com>
2025-08-07 20:39:31 +0200

    basic server template

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"gonas/internal/handler"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":8080"
+	}
+
+	http.HandleFunc("/upload", handler.Upload)
+	http.HandleFunc("/download", handler.Download)
+	http.HandleFunc("/list", handler.List)
+	http.HandleFunc("/delete", handler.Delete)
+
+	log.Printf("Starting server on http://localhost%s\n", port)
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- internal/handler/list.go
+++ internal/handler/list.go
@@ -1,1 +1,7 @@
 package handler
+
+import "net/http"
+
+func List(w http.ResponseWriter, r *http.Request) {
+
+}
