7254ea60adeff917a60bf94a907d0311ccc07202
Jodus-Melodus <philipgricius@gmail.com>
2025-08-10 15:35:36 +0200

    fix folder path link

--- web/static/script.js
+++ web/static/script.js
@@ -1,25 +1,3 @@
-document.querySelectorAll(".delete-button").forEach(button => {
-    button.addEventListener("click", () => {
-        const name = button.dataset.name;
-
-        fetch("/delete", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ name })
-        })
-            .then(res => {
-                if (res.ok) {
-                    alert("Delete successful!");
-                    location.reload();
-                } else {
-                    alert("Delete failed.");
-                }
-            })
-            .catch(err => console.error(err));
-    })
-});
 
 document.addEventListener("DOMContentLoaded", () => {
     const logoutLink = document.getElementById("logout-link");
